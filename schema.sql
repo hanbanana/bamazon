e270f6b03198b4535a44a090a7445271efcfaed7
Han <2han@live.com>
2018-07-11 20:52:31 -0700

    add js

---
+++ schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS Bamazon;
+
+CREATE DATABASE Bamazon;
+
+USE Bamazon;
+
+CREATE TABLE products (
+    item_id INTEGER(50) AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(50) NOT NULL,
+    department_name VARCHAR(50) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    stock_quantity INTEGER(50) NOT NULL,
+    PRIMARY KEY (item_id)
+    
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ('Banana', 'Food', 0.20, 40),
+    ('Avocado', 'Food', 0.99, 50),
+    ('Band Aid', 'Pharmacy', 4.99, 100),
+    ('Tylenol', 'Pharmacy', 5.99, 200),
+    ('Nike Shoes', 'Apparel', 149.99, 20),
+    ('Levis Jeans', 'Apparel', 49.99, 30),
+    ('Shampoo', 'Cosmetics', 6.99, 70),
+    ('Conditioner', 'Cosmetics', 7.50, 90);
